af5f1fedb27f3fa66c7f6e1d3b34700cce185be0
Simon Walker <github@helpmebot.org.uk>
2011-09-18 14:27:44 +0100

    fixup

--- BeebMaze/ol.cs
+++ BeebMaze/ol.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Tao.OpenGl;
+
 namespace BeebMaze
 {
     public partial class ol : Form
@@ -14,10 +16,34 @@ namespace BeebMaze
         public ol()
         {
             InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
         }
 
         private void simpleOpenGlControl1_Load(object sender, EventArgs e)
         {
+            // Clear the window
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+
+            // Specify polygon to be drawn and vertices of polygon
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glColor3f(1f, 1f, 1f);
+            Gl.glVertex2f(-0.5f, -0.5f);
+            Gl.glColor3f(1f, 0f, 0f);
+            Gl.glVertex2f(-0.5f, 0.5f);
+            Gl.glColor3f(0f, 1f, 0f);
+            Gl.glVertex2f(0.5f, 0.5f);
+            Gl.glColor3f(0f, 0f, 1f);
+            Gl.glVertex2f(0.5f, -0.5f);
+            Gl.glEnd();
+
+            // Flush the buffer to force drawing of all objects thus far
+            Gl.glFlush();
+
+        }   
+
+        ~ol()
+        {
+            simpleOpenGlControl1.DestroyContexts();
         }
     }
 }
